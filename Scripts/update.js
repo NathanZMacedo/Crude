85ca6991c4739a597bb4250e63487a223f94b35a
NathanZMacedo <nathanzmacedo@gmail.com>
2023-04-20 16:45:50 -0300

    função de atualizar o form

--- Scripts/update.js
+++ Scripts/update.js
@@ -2,9 +2,14 @@
 // PATCH MODIFICA APENAS OS ATRIBUTOS ENVIADOS
 // PUT MODIFICCA O OBJETO INTEIRO
 
-var editMode = document.getElementById("submit").innerHTML === "Edit Aqui!"
+var editMode = document.getElementById("submit").innerHTML === "Edit Aqui!";
 
 function EnviandoDadosAtuais(id,nameItem,emailItem){
     document.getElementById("email").value = emailItem;
     document.getElementById("name").value = nameItem;
+
+    document.getElementById('submit').setAttribute("data-id", id);
+    document.getElementById('submit').innerHTML = "Edit Aqui!";
+    editMode = document.getElementById("submit").innerHTML === "Edit Aqui!";
+    // console.log(editMode)
 }
