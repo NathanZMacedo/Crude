3d377957172954c820396676ac60bda4f1c4ca79
NathanZMacedo <nathanzmacedo@gmail.com>
2023-04-20 17:11:50 -0300

    criando a função de editar com backend

--- Scripts/update.js
+++ Scripts/update.js
@@ -1,3 +1,4 @@
+const urlDelete = "https://crud-javascript-ff277-default-rtdb.firebaseio.com/pessoas/"
 // DIFERENÇA DE PATCH VS PUT
 // PATCH MODIFICA APENAS OS ATRIBUTOS ENVIADOS
 // PUT MODIFICCA O OBJETO INTEIRO
@@ -12,4 +13,29 @@ function EnviandoDadosAtuais(id,nameItem,emailItem){
     document.getElementById('submit').innerHTML = "Edit Aqui!";
     editMode = document.getElementById("submit").innerHTML === "Edit Aqui!";
     // console.log(editMode)
+}
+
+async function editData(id){
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const data ={
+        name: name,
+        email: email,
+    }
+    responde = await fetch(`${urlEdit}${id}.json`,{
+        method:'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(res=>{
+            PegarDados();
+            documents.getElementById("submit").removeAttribute("data-id");
+            document.getElementById('email').value="";
+            document.getElementById('name').value="";
+            document.getElementById('submit').innerText="Cadastrar!";
+            editMode= document.getElementById("submit").innerHTML === "Edit Aqui!";
+        })
+            .catch(err=>cpnsole.log("erro ao editar",err));
 }
